0ae875bb0cd72c7a8f07b5432b254954848cc3b3
200387807 <46694599+200387807@users.noreply.github.com>
2019-05-27 15:20:42 -0400

    Add project files.

---
+++ week4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(week4.Startup))]
+namespace week4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
